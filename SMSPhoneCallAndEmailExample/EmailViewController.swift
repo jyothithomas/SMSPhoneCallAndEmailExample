13dc408845f30bbbacbc8bb10738d43ffa26611e
jyothithomas <60137888+jyothithomas@users.noreply.github.com>
2020-03-25 18:45:22 -0400

    [JT] Email changed

--- SMSPhoneCallAndEmailExample/EmailViewController.swift
+++ SMSPhoneCallAndEmailExample/EmailViewController.swift
@@ -28,7 +28,7 @@ class EmailViewController: UIViewController, MFMailComposeViewControllerDelegate
         {
             let picker = MFMailComposeViewController()
             picker.mailComposeDelegate = self
-            picker.setToRecipients(["pritam.world@gmail.com"])
+            picker.setToRecipients(["jyothithomas.05@gmail.com"])
             picker.setSubject(subject.text!)
             picker.setMessageBody(body.text!, isHTML: true)
             
